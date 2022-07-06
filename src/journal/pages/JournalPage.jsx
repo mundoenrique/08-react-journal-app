f08e5cd8670ed455c88e5a9e8aebb5bfd3e3365f
mundoenrique <mundoenrique@hotmail.com>
2022-07-06 17:17:28 -0400

    diseño botón agregar

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,3 +1,5 @@
+import { AddOutlined } from '@mui/icons-material';
+import { IconButton } from '@mui/material';
 import JournalLayout from '../layout/JournalLayout';
 import { NoteView, NothingSelectedView } from '../views';
 
@@ -5,8 +7,21 @@ export function JournalPage() {
 	return (
 		<>
 			<JournalLayout>
-				{/* <NothingSelectedView /> */}
-				<NoteView />
+				<NothingSelectedView />
+				{/* <NoteView /> */}
+				<IconButton
+					size="large"
+					sx={{
+						color: 'white',
+						backgroundColor: 'error.main',
+						':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+						position: 'fixed',
+						right: 50,
+						bottom: 50,
+					}}
+				>
+					<AddOutlined sx={{ fontSize: 30 }} />
+				</IconButton>
 			</JournalLayout>
 		</>
 	);
