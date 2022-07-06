edde7f7b43b9c24da8659df730f88de335d7d6e4
mundoenrique <mundoenrique@hotmail.com>
2022-07-06 12:43:41 -0400

    COnfigura material

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import AppRouter from './router/AppRouter';
+import { AppTheme } from './theme';
 
 export default function App() {
-	return <AppRouter />;
+	return (
+		<AppTheme>
+			<AppRouter />
+		</AppTheme>
+	);
 }
