c9a0329187d70e93b89792b8327e9decc7453e2e
mundoenrique <mundoenrique@hotmail.com>
2022-07-06 16:06:22 -0400

    Diseño SideBar

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,5 +1,5 @@
-import { Box } from '@mui/material';
-import { NavBar } from '../components';
+import { Box, Toolbar } from '@mui/material';
+import { NavBar, SideBar } from '../components';
 
 const drawerWidth = 240;
 
@@ -7,7 +7,9 @@ export default function JournalLayout({ children }) {
 	return (
 		<Box sx={{ display: 'flex' }}>
 			<NavBar drawerWidth={drawerWidth} />
+			<SideBar drawerWidth={drawerWidth} />
 			<Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+				<Toolbar />
 				{children}
 			</Box>
 		</Box>
